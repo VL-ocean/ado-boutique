812dd53ed1bb7852d710a8780abad9343d135e45
Viktoriia Liakhovetska <vliakhovetska@gmail.com>
2024-10-04 12:50:01 +0000

    Add stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Q6AfyRrlP3sNOeVqUksMBYcUSncN3kSnVayMUU24OEQEHKAiCFZG3a2AURJOwBzISLkvOxipCNEv81AhC54ka5C00qdeGyJ6F',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
