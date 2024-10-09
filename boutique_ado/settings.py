d3028dd21564741fd5c9a592d66945085bf2f105
Viktoriia Liakhovetska <vliakhovetska@gmail.com>
2024-10-09 11:31:42 +0000

    Set debug

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     "8000-vlocean-adoboutique-agx1294v6sh.ws.codeinstitute-ide.net",
